aeb7cd348f674e5bf40cb886e7f6f4dd5919a32c
Gary Ritchie <gary@rstudio.com>
2021-04-28 16:45:14 -0700

    add zsh alias for switching to jdk15

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,3 @@
 alias cls='clear && tmux clear-history'
 alias jdk8='export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home'
+alias jdk15='export JAVA_HOME=/usr/local/Cellar/openjdk/15.0.2'
