166a070fad05445bce8bd4674acd8cc55225e8e2
Gary Ritchie <gary@rstudio.com>
2021-11-28 14:50:50 -0800

    add aliases for M1 JDKs

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,5 @@
 alias cls='clear && tmux clear-history'
-alias jdk8='export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home'
-alias jdk11='export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home'
-alias jdk15='export JAVA_HOME=/usr/local/opt/openjdk@15'
+#alias jdk8='export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home'
+#alias jdk11='export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home'
+alias jdk11='export JAVA_HOME=/opt/homebrew/opt/openjdk@11'
+alias jdk17='export JAVA_HOME=/opt/homebrew/opt/openjdk@17'
