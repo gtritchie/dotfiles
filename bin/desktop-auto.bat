9dc32c9d781fcf8b16d78bccad098cce71573cd1
Gary Ritchie <gary@posit.co>
2024-05-20 09:29:02 -0600

    fix path sep on windows

--- bin/desktop-auto.bat
+++ bin/desktop-auto.bat
@@ -2,7 +2,7 @@
 
 set RSTUDIO_WHICH_R=C:\R\R-3.6.3\bin\R.exe
 set PYTHONPATH=%USERPROFILE%\rstudio-ide-automation\rstudio_server_pro
-set CHROMEDRIVER_PATH=%USERPROFILE%/bin/chromedriver
+set CHROMEDRIVER_PATH=%USERPROFILE%\bin\chromedriver.exe
 set ELECTRON_PATH="C:\Program Files\RStudio\rstudio.exe"
 set RS_NO_SPLASH=1
 
