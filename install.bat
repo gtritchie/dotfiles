f76e1dc5ff73b2e85419909c8b23c8bff979a137
Gary Ritchie <gary@posit.co>
2024-05-21 10:39:55 -0600

    windows setup batch file

---
+++ install.bat
@@ -0,0 +1,7 @@
+@echo off
+
+git config --global core.excludesfile ~/.gitignore_global
+git config --global user.name "Gary Ritchie"
+git config --global user.email "gary@posit.co" 
+git config --global core.editor "vim"
+git config --global init.defaultBranch main
